ecbb0e53d6e5e60e1266992cba6baaa46deefa3b
tehila <tehilapelled16@gmail.com>
2019-09-06 10:30:05 +0300

    all

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+
+// // $(".pic_gallery").click(function(){
+// //                          $(this).animate({
+
+// //                              height: "500px",
+// //                              width:  "500px"
+// //                          },50);
+// // });
+
+// var small={width: "157.5px",height: "210px"};
+// var large={width: "400px",height: "232px"};
+// var count=1; 
+// $(".pic_gallery").css(small).on('click',function () { 
+//             $(this).animate((count==1)?large:small);
+//             count = 1-count;
+//         });
